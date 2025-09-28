6130f338c5bf8aebce299d6f424892130b3c794e
Abdul Basit <ab9133711@gmail.com>
2025-09-28 12:41:40 +0500

    add fetch all products after creating invoice

--- client/src/components/homePageComponents/sales/InvoiceComponent.jsx
+++ client/src/components/homePageComponents/sales/InvoiceComponent.jsx
@@ -4,6 +4,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
+  setAllProducts,
   setSearchQuery,
   setSearchQueryProducts,
   setSelectedItems,
@@ -398,6 +399,13 @@ const InvoiceComponent = () => {
         setIsInvoiceGenerated(true);
         fetchLastBillNo(billType)
 
+        if (response) {
+          const allProductsBefore = await config.fetchAllProducts();
+          if (allProductsBefore.data) {
+            dispatch(setAllProducts(allProductsBefore.data));
+          }
+        }
+
       } catch (error) {
         console.error('Failed to generate bill', error.response.data)
         const errorMessage = extractErrorMessage(error)
@@ -505,6 +513,8 @@ const InvoiceComponent = () => {
 
     const billId = billNo || ""
 
+    if (!userConfirmed) return;
+
     if (userConfirmed) {
       setIsLoading(true)
       console.log(selectedItems)
@@ -564,6 +574,13 @@ const InvoiceComponent = () => {
         }
         setViewBillNo(billNo)
         fetchLastBillNo(billType)
+
+        if (response) {
+          const allProductsBefore = await config.fetchAllProducts();
+          if (allProductsBefore.data) {
+            dispatch(setAllProducts(allProductsBefore.data));
+          }
+        }
 
       } catch (error) {
         console.error('Failed to generate bill', error.response.data)
